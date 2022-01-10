18e4e5d0718e76df4a5f747f9cde8d401a6a040c
Francois Kritzinger <francois@codesynthesis.com>
2022-01-10 13:34:56 +0200

    Test

--- tests/basics/driver.cxx
+++ tests/basics/driver.cxx
@@ -1,7 +1,34 @@
 #include <double-conversion/double-conversion.h>
 
-int main ()
+using namespace double_conversion;
+
+int
+main ()
 {
-  using namespace std;
+  // String to double.
+  //
+  {
+    int n (0);
+    StringToDoubleConverter c (
+        StringToDoubleConverter::NO_FLAGS, 0, 0, nullptr, nullptr);
+
+    if (c.StringToDouble ("0.123", 5, &n) != 0.123)
+      return 1;
+  }
+
+  // Double to string.
+  //
+  {
+    char b[32];
+    StringBuilder sb (b, sizeof (b));
+    DoubleToStringConverter c (
+        StringToDoubleConverter::NO_FLAGS, nullptr, nullptr, 'e', -6, 21, 6, 0);
+
+    if (!c.ToShortest (0.123, &sb))
+      return 1;
+    if (strcmp (sb.Finalize (), "0.123") != 0)
+      return 1;
+  }
+
   return 0;
 }
